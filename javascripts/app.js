cafea67d177fd5988167b73c78b6c8c5755be06f
Bernard Tatin <bernard.tatin@outlook.fr>
2016-11-01 18:18:48 +0100

    Clean code

--- javascripts/app.js
+++ javascripts/app.js
@@ -5,7 +5,6 @@ var marcel_kernel = (function () {
         jsRoot: 'bright-marcel-kernel/javascripts'
     },
         appVariables = {
-            // config: 'gitio/pages/config.js',
             main_code: 'private/main-purejs.js',
             libs: ['public/joose.min.js', 'private/utils.js', 'private/myajax.js', 'private/purejs-lib.js', 'private/jprint.js'],
             libname: 'pure Javascript 0.1.1',
@@ -22,12 +21,10 @@ var marcel_kernel = (function () {
         },
         app_loader: function () {
             appVariables.navigator = navigator.appName + ' ' + navigator.appCodeName + ' ' + navigator.appVersion;
-            // LazyLoad.js(appVariables.config, function () {
-                LazyLoad.js(appVariables.libs.map(normalize_libname), function () {
-                    LazyLoad.js(normalize_libname(appVariables.main_code), function () {
-                    });
+            LazyLoad.js(appVariables.libs.map(normalize_libname), function () {
+                LazyLoad.js(normalize_libname(appVariables.main_code), function () {
                 });
-            // });
+            });
         }
     };
 })();
