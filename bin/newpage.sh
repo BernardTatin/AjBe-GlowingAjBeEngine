faaf2012fd766fd1ae715dd19e16a963177956d3
bernard (NetBSD) tatin <bernard.tatin@outlook.fr>
2019-12-13 17:09:00 +0100

    A better newpage.sh which modifiy content.html

--- bin/newpage.sh
+++ bin/newpage.sh
@@ -14,7 +14,12 @@ DOHELP
 }
 
 dopage () {
-    cat > $1 <<DOPAGE
+    local dp_page_name=$1
+    local dp_title=
+    shift
+    dp_title="$@"
+    mkdir -p ${pages_base}/$(dirname $dp_page_name)
+    cat > ${pages_base}/$dp_page_name <<DOPAGE
 <!DOCTYPE html>
 <html>
 
@@ -25,6 +30,8 @@ dopage () {
     </head>
 
 	<body>
+        <!-- file : $dp_page_name -->
+        <!-- title: ${dp_title} -->
         <!-- write some clever text here -->
 	</body>
 </html>
@@ -32,10 +39,30 @@ DOPAGE
 }
 
 addtomenu () {
-    cat <<ADDTOMENU
-WARNING: not implemented, sorry
-WARNING: you must add a line in the file:
-            $(dirname $1)/content.html
+    local am_page_name=$1
+    local am_title=
+    shift
+    am_title="$*"
+    new_line="<li><p href='index.html?page=$(basename $am_page_name)&root=$(dirname $am_page_name)'>$am_title</p></li>"
+    content_file=${pages_base}/$(dirname $am_page_name)/content.html
+    grep -Fhqs "$new_line" $content_file \
+        && echo "already in menu" \
+        && return 0
+    cp $content_file ${content_file}.bak
+    cat > $content_file <<ADDTOMENU
+<html>
+    <head>
+        <meta http-equiv="Pragma" content="no-cache" />
+        <meta http-equiv="Expires" content="-1" />
+        <meta charset='utf-8' />
+    </head>
+    <body>
+        <ul style="display: block;">
+$(grep -Fhs "<li><p href=" ${content_file}.bak)
+            $new_line
+        </ul>
+    </body>
+</html>
 ADDTOMENU
 }
 
@@ -49,18 +76,18 @@ case $# in
                 dohelp
                 ;;
             *)
-                dopage $1 \
-                    && addtomenu $1 "No title"
-                exit 0
+                page_name="$1"
+                shift
+                title='No Title'
                 ;;
         esac
 		;;
-    2)
-        dopage $1 \
-            && addtomenu $1 $2
+    *)
+        page_name="$1"
+        shift
+        title="$*"
         ;;
-	*)
-        echo "ERROR: too much arguments"
-        exit 1
-		;;
 esac
+
+dopage "$page_name" "$title" \
+    && addtomenu "$page_name" "$title"
