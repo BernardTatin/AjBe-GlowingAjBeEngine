848d3db2c22a731511f44bc7fba18ed451974c84
bernard (pop) tat1 <bernard.tatin@outlook.fr>
2020-02-27 12:20:58 +0100

    Great test on prototype inheritance

--- javascripts/tests/proto-inherit.js
+++ javascripts/tests/proto-inherit.js
@@ -7,3 +7,49 @@
 
 "use strict";
 
+function Animal(theSpecie, theName) {
+    this.specie = theSpecie;
+    this.name = theName;
+}
+/*
+Animal.prototype.getSpecie = function() {
+    return this.specie;
+};
+*/
+
+Animal.prototype = {
+    getName: function() {
+        return this.name;
+    },
+    print: function() {
+        console.log(this.getName()
+            + ' is a '
+            + this.getSpecie());
+    },
+    getSpecie: function () {
+        return this.specie;
+    }
+}
+
+function Cat(theName) {
+    Animal.call(this, 'Cat');
+    this.name = theName;
+}
+
+function Dog(theName) {
+    Animal.call(this, 'Dog');
+    this.name = theName;
+}
+Cat.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+let cat = new Cat('Plume');
+let dog = new Dog('Hailey');
+let friends = [cat, dog];
+
+for (var i=0, l=friends.length; i<l; i++) {
+    let f = friends[i];
+    console.log('-----------------------');
+    f.print();
+}
+
