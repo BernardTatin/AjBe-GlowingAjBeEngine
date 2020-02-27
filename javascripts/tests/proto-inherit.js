1fc34ab5f894e926d306204688b528a7c7de8c50
bernard (pop) tat1 <bernard.tatin@outlook.fr>
2020-02-27 13:19:14 +0100

    here is the real good and full test

--- javascripts/tests/proto-inherit.js
+++ javascripts/tests/proto-inherit.js
@@ -11,13 +11,19 @@ function Animal(theSpecie, theName) {
     this.specie = theSpecie;
     this.name = theName;
 }
-/*
 Animal.prototype.getSpecie = function() {
     return this.specie;
-};
-*/
+}
+Animal.prototype.getName = function() {
+    return this.name;
+}
+Animal.prototype.print = function() {
+    console.log(this.getName()
+        + ' is a '
+        + this.getSpecie());
+}
 
-Animal.prototype = {
+/* Animal.prototype = {
     getName: function() {
         return this.name;
     },
@@ -29,19 +35,40 @@ Animal.prototype = {
     getSpecie: function () {
         return this.specie;
     }
-}
+} */
 
 function Cat(theName) {
-    Animal.call(this, 'Cat');
-    this.name = theName;
+    Animal.call(this, 'Cat', theName);
 }
 
 function Dog(theName) {
-    Animal.call(this, 'Dog');
-    this.name = theName;
+    Animal.call(this, 'Dog', theName);
 }
 Cat.prototype = Object.create(Animal.prototype);
 Dog.prototype = Object.create(Animal.prototype);
+
+
+Dog.prototype.talk = function() {
+    return 'Ouaf';
+}
+Dog.prototype.print = function() {
+    console.log(this.getName()
+        + ' is a '
+        + this.getSpecie()
+        + ' and shout '
+        + this.talk());
+}
+
+Cat.prototype.shout = function() {
+    return "it's the night...";
+}
+Cat.prototype.print = function() {
+    console.log(this.getName()
+        + ' is a '
+        + this.getSpecie()
+        + ' and repeat '
+        + this.shout());
+}
 
 let cat = new Cat('Plume');
 let dog = new Dog('Hailey');
@@ -51,5 +78,6 @@ for (var i=0, l=friends.length; i<l; i++) {
     let f = friends[i];
     console.log('-----------------------');
     f.print();
+    // f.nice_print();
 }
 
